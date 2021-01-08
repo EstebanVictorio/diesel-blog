50df91c6ad4c615f317a8c4efe27346229f140f8
Esteban Victorio <estebanvictorio92@gmail.com>
2021-01-07 22:11:30 -0800

    feature: add errors module, error parsing from diesel to custom ones and added the create_user transaction function into the models mod

---
+++ src/models.rs
@@ -0,0 +1,25 @@
+use crate::errors::AppError;
+use crate::schema::*;
+use diesel::prelude::*;
+
+type Result<T> = std::result::Result<T, AppError>;
+
+#[derive(Queryable, Identifiable, Serialize, Debug, PartialEq)]
+pub struct User {
+  pub id: i32,
+  pub username: String,
+}
+
+pub fn create_user(conn: &SqliteConnection, username: &str) -> Result<User> {
+  conn.transaction(|| {
+    diesel::insert_into(users::table)
+      .values((users::username.eq(username)))
+      .execute(conn)?;
+
+    users::table
+      .order(users::id.desc())
+      .select((users::id, users::username))
+      .first(conn)
+      .map_err(Into::into)
+  })
+}
